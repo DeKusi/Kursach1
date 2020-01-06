d6dc4f61b93adb77c2381672cdf419c6f2769950
Ксения <k953159@yandex.ru>
2020-01-06 15:21:37 +0300

    "Attempt 1"

--- Реализация/Kursach/newApp/views.py
+++
@@ -1,5 +0,0 @@
-from django.shortcuts import render
-
-
-def indexprob(request):
-    return render(request, 'newApp/homePage.html')
--- Реализация/untitled1/riddles/forms.py
+++
@@ -1,8 +0,0 @@
-from django.forms import ModelForm
-from .models import *
-
-
-class PlaneForm(ModelForm):
-    class Meta:
-        model = plane
-        exclude = [""]
